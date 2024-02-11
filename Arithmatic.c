412a5b44d0a0224b350233a6f85193a501798d48
pavi0426 <149355279+pavi0426@users.noreply.github.com>
2024-02-11 13:39:32 +0530

    Add files via upload

---
+++ Arithmatic.c
@@ -0,0 +1,43 @@
+#include<stdio.h>
+
+// Arithmetic functions
+double add(double a, double b) {
+    return a + b;
+}
+
+double subtract(double a, double b) {
+    return a - b;
+}
+
+double multiply(double a, double b) {
+    return a * b;
+}
+
+double divide(double a, double b) {
+    if (b != 0) {
+        return a / b;
+    } else {
+        printf("Error: Division by zero\n");
+        return 0; // Not a Number
+    }
+}
+
+double power(double base, double exponent) {
+      double result = 1.0;
+
+    if (exponent >= 0) {
+        for (int i = 0; i < exponent; ++i) {
+            result *= base;
+        }
+    } else {
+        for (int i = 0; i > exponent; --i) {
+            result /= base;
+        }
+    }
+
+    return result;
+}
+int main(){
+printf("succuess");
+
+}
